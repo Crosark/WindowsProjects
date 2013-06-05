9dfd1b5831cc2d85605bdf2a4c86434fbf63e59d
Asim Mittal <asim.mittal@gmail.com>
2013-06-05 04:29:58 -0700

    A few necessary changes to ChatHeadsSample
    Now you can provide the image path and notification label text along
    with radius for the chat head control

--- ChatheadSample/ChatheadSample/ChatHead.xaml.cs
+++ ChatheadSample/ChatheadSample/ChatHead.xaml.cs
@@ -8,13 +8,31 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace UserControls
 {
     public partial class ChatHead : UserControl
     {
-        //Create a property called 'Radius'
+        const double thickness = 1.5;
         public int Radius { get; set; }
+        public string Label {
+            get { return txtNotification.Text; }
+            set { txtNotification.Text = value; }
+        }
+
+        private string _imgPath;
+        public string ImagePath {
+            get {
+                return _imgPath; 
+            }
+            set{
+                _imgPath = value;
+                Uri uri = new Uri(_imgPath, UriKind.RelativeOrAbsolute);
+                BitmapImage bmp = new BitmapImage(uri);
+                image.Source = bmp;         
+            }
+        }
 
         /// <summary>
         /// Class constructor
@@ -26,20 +44,31 @@ namespace UserControls
         /// <summary>
         /// This creates the circular clip around this control
         /// </summary>
-        private void initAppearance() {
-            
-            //decide the center of the circular region
-            double centerX = this.Height / 2;
-            double centerY = this.Width / 2;
-            
-            //create the clip geometry
-            EllipseGeometry geom = new EllipseGeometry();
-            geom.RadiusY = Radius;
-            geom.RadiusX = Radius;
-            geom.Center = new Point(centerX, centerY);
-            
-            //apply the clip to this control
-            this.Clip = geom;
+        private void invalidate() {
+
+            if (Radius >= 0 && Radius * 2 <= Width) {
+                
+                //decide the center of the circular region
+                double centerX = this.Height / 2;
+                double centerY = this.Width / 2;
+
+                //create the clip geometry
+                EllipseGeometry geom = new EllipseGeometry();
+                geom.RadiusY = Radius - thickness;
+                geom.RadiusX = Radius - thickness;
+                geom.Center = new Point(centerX, centerY);
+
+                //apply the clip to this control
+                image.Clip = geom;
+
+                //now setup the shape outline
+                outline.Width = Radius * 2;
+                outline.Height = Radius * 2;
+
+                //set the location of the notification
+                double coord = (Width / 1.732) - Radius;
+                notification.Margin = new Thickness(coord, coord, 0, 0);
+            }
         }
 
         /// <summary>
@@ -48,7 +77,17 @@ namespace UserControls
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void UserControl_Loaded_1(object sender, RoutedEventArgs e) {
-            initAppearance();
+            invalidate();
+        }
+
+        /// <summary>
+        /// Invoked when thie control is resized
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UserControl_SizeChanged_1(object sender, SizeChangedEventArgs e) {
+            Height = Width;
+            invalidate();
         }
     }
 }
